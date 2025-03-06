8b6391c2de976459628c0ecbd85b78904e5a2c82
Nicole Vazquez <jeannicoletobar@gmail.com>
2025-03-06 13:44:35 +0100

    Añadido sweetalert desde instalacion y desde script en index.html para poder hacer un alert de cuanta eliminada

--- src/Close/Close.jsx
+++ src/Close/Close.jsx
@@ -1,52 +1,58 @@
-import { useRef } from "react"
-import "../App.css"
+import { useRef } from "react";
+import "../App.css";
 
+function Close({ currentAccount, accounts, setAccount }) {
+  const inputUser = useRef();
+  const inputPin = useRef();
 
-function Close({currentAccount, accounts, setAccount}){
-  const inputUser = useRef()
-  const inputPin = useRef()
- 
+  const handleCloseAccount = function (e) {
+    e.preventDefault();
 
-  const handleCloseAccount = function(e) {
-    e.preventDefault()
-    
-    
-    const user = inputUser.current.value
-    const pin = Number(inputPin.current.value)
+    const user = inputUser.current.value;
+    const pin = Number(inputPin.current.value);
 
-    
-    
-    
-    if(user === currentAccount.username && pin === currentAccount.pin){
-      const index = accounts.findIndex((acc) => acc.username === user && acc.pin === pin)
-      accounts.splice(index, 1)
-      setAccount(null)
-      console.log(accounts, "Cuenta cerrada exitosamente")
-      
+    if (user === currentAccount.username && pin === currentAccount.pin) {
+      const index = accounts.findIndex(
+        (acc) => acc.username === user && acc.pin === pin,
+      );
+      accounts.splice(index, 1);
+      setAccount(null);
 
-
+      // Mostrar un pop out de confirmación con sweetAlert
+      swal({
+        title: "Cuenta eliminada",
+        text: "La cuenta ha sido eliminada exitosamente",
+        icon: "success",
+        button: "Aceptar",
+      });
     }
-
-
-  }
-    return(
-      
-        <div className="operation operation--close">
-        <h2>Close account</h2>
-        <form className="form form--close">
-          <input type="text" className="form__input form__input--user" ref={inputUser} />
-          <input
-            type="password"
-            maxlength="6"
-            className="form__input form__input--pin"
-            ref={inputPin}
-          />
-          <button onClick={handleCloseAccount} className="form__btn form__btn--close">&rarr;</button>
-          <label className="form__label">Confirm user</label>
-          <label className="form__label">Confirm PIN</label>
-        </form>
-      </div>
-    )
+  };
+  return (
+    <div className="operation operation--close">
+      <h2>Close account</h2>
+      <form className="form form--close">
+        <input
+          type="text"
+          className="form__input form__input--user"
+          ref={inputUser}
+        />
+        <input
+          type="password"
+          maxlength="6"
+          className="form__input form__input--pin"
+          ref={inputPin}
+        />
+        <button
+          onClick={handleCloseAccount}
+          className="form__btn form__btn--close"
+        >
+          &rarr;
+        </button>
+        <label className="form__label">Confirm user</label>
+        <label className="form__label">Confirm PIN</label>
+      </form>
+    </div>
+  );
 }
 
-export default Close
+export default Close;
